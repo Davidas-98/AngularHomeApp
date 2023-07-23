1849288ccfff17373b15cd5f6530a94599a1b414
Jade <davidas98_@hotmail.com>
2023-07-23 21:23:31 +0200

    Lesson 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+let template = `
+<main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Default</h1>`,
+  imports: [HomeComponent],
+  template: template,
   styleUrls: ['./app.component.css'],
 })
+
+
+
 export class AppComponent {
-  title = 'default';
+  title = 'Homes';
 }
